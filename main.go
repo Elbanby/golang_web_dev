94958f1d7621354464c1585dc2ee5b1f5c99cadb
omarelbanby <obanby@gmail.com>
2020-02-20 13:14:45 -0500

    Add mux router

--- main.go
+++ main.go
@@ -3,27 +3,52 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func simpleHandler(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1> Home </h1>")
+}
 
-	switch r.URL.Path {
-	case "/":
-		fmt.Fprint(w, "<h1>Welcome home!</h1>")
-	case "/contact":
-		fmt.Fprint(w, `
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, `
 						<h1>Contact info:</h1>
 						email: <a href="mailto:support@mail.com">Support</a>
-		`)
-	default:
-		fmt.Fprint(w, `<h1>404</h1>`)
-	}
+	`)
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, `
+		<h1>FAQ(s)</h1>
+		<h3> How much do you charge ?</h3>
+		<p> $160/hour </p>
+	`)
+}
+
+func pageNotFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, `<h1>You are lost</h1>`)
 }
 
 func main() {
-	http.HandleFunc("/", simpleHandler)
-	if err := http.ListenAndServe(":9000", nil); err != nil {
+	// Creates a new router
+	r := mux.NewRouter()
+
+	// Assign functions to handle different paths
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+
+	// Converts pageNotFound func to type HandlerFunc
+	notFound := http.HandlerFunc(pageNotFound)
+	r.NotFoundHandler = notFound
+
+	// Start up the server
+	if err := http.ListenAndServe(":9000", r); err != nil {
 		fmt.Println("Error occurred while binding to port", err)
 	}
 
